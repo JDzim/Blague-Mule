e002fa9b51488311061df2b9007d423f799f11c1
JDzim <joseph.dzimbalka@gmail.com>
2014-03-17 15:45:29 +0100

    scripts

--- BlagueMule/scripts/compile.sh
+++ BlagueMule/scripts/compile.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#javac 	-d 	../build/classes/blague 	../src/blague/*.java
-#javac 	-d 	../build/classes/client 	../src/client/*.java
-#javac 	-d 	../build/classes/codebase 	../src/codebase/*.java
-#javac 	-d 	../build/classes/exceptions 	../src/exceptions/*.java
-#javac 	-d 	../build/classes/serveur 	../src/serveur/*.java
-
 cd ../src2
 
+echo ""
+echo "PACKAGE IHM"
+echo ""
+javac 	-d 	../build/classes 	*.java
 echo ""
 echo "PACKAGE BLAGUE"
 echo ""
@@ -19,6 +17,6 @@ javac 	-d 	../build/classes 	codebase/*.java
 echo ""
 echo "PACKAGE PROVIDER"
 echo ""
-javac 	-d 	../build/classes 	provider/*.java
+javac 	-d	../build/classes	provider/*.java
 echo ""
 
